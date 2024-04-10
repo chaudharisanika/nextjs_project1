544b481fd033dff4a98fcd1e5001d07b0abe44c6
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2024-04-10 19:35:40 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //for taking the images other than own domain
-  images:{
-    remotePatterns:[
-      {protocol:"https",hostname:"www.dominos.co.in"}
-    ]
-  }
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "www.dominos.co.in" }],
+  },
 };
 
 export default nextConfig;
--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -1,43 +1,76 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React, { useContext, useState ,useEffect} from "react";
 
-const Card = () => {
+function Card(props) {
+  const data = props.foodData;
+  const priceOptions = Object.keys(data.price);
+  const [size, setSize] = useState(priceOptions[0]); // Initialize size state 
+  const [qty, setQty] = useState(1);
+
+
+  const handleQty = (e) => {
+    setQty(e.target.value);
+  };
+
+  const handleSize = (e) => {
+    setSize(e.target.value);
+  };
+  
+  let finalPrice = qty * parseInt(data.price[size]);
+
   return (
     <div className="box">
-        <div className="w-80 rounded-lg bg-white overflow-hidden dark:bg-black border-gradient">
+      <div className="w-80 rounded-lg bg-white overflow-hidden dark:bg-black border-gradient">
+        {/* <Link href={{ pathname: "/Item/[item]" }} as={`Item/${data["_id"]}`}> */}
           <div className="relative w-full h-80">
-            {/* <Image src={data.img} layout="fill" objectFit="cover" alt="pizza" /> */}
+            <Image src={data.img} layout="fill" objectFit="cover" alt="pizza" />
           </div>
-        </div>
-        <div className="p-4">
-            <div lassName="font-bold mb-2 text-xl uppercase"> Food name</div>
+          <div className="p-4">
+            <div className="font-bold mb-2 text-xl uppercase"> {data.name}</div>
             <p className=" short_description text-gray-700 dark:text-gray-400 text-base">
-              description
+              {data.description}
             </p>
-        </div>
+          </div>
+        {/* </Link> */}
         <div className="flex px-4 justify-between">
           <select
             className=" h-100  p-1 text-black hover:font-bold font-semibold cursor-pointer dark:text-gray-300  border border-black dark:border-gray-400 rounded"
+            onChange={handleQty}
           >
-            
+            {Array.from(Array(6), (e, i) => {
+              return (
+                <option key={i + 1} value={i + 1}>
+                  {i + 1}
+                </option>
+              );
+            })}
           </select>
           <select
             className=" h-100  p-1 text-black hover:font-bold font-semibold cursor-pointer dark:text-gray-300  border border-black dark:border-gray-400 rounded"
-            
+            onChange={handleSize}
           >
-            
+            {priceOptions.map((options) => {
+              return (
+                <option className="uppercase" key={options} value={options}>
+                  {options}
+                </option>
+              );
+            })}
           </select>
         </div>
         <div className="flex p-4 font-bold  justify-between">
           <button
-            
+
             className="border dark:border-gray-400 border-gray-900 rounded p-2 hover:bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700  hover:text-gray-100 "
           >
             Add to cart
           </button>
-          <p className="p-2 text-xl">₹90/-</p>
-          </div>
+          <p className="p-2 text-xl">Rs.{finalPrice}/-</p>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
